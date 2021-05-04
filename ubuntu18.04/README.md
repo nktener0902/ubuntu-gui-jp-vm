04d2a54236dd789dee9c94de2d8e89c7c5e508e2
Yu Nakata <y-nakata@outlook.com>
2021-05-04 16:16:02 +0900

    Initial commit

---
+++ ubuntu18.04/README.md
@@ -0,0 +1,9 @@
+
+* Ubuntu18.04
+* GNOME
+* Japanese
+
+```bash
+ansible-galaxy install robertdebock.gnome
+ansible-galaxy install gantsign.keyboard
+```
---
+++ ubuntu18.04/bootstrap.sh
@@ -0,0 +1,18 @@
+#/bin/bash -eux
+
+if type ansible-galaxy > /dev/null 2>&1
+then
+    ansible-galaxy install robertdebock.gnome
+    ansible-galaxy install gantsign.keyboard
+else
+    echo "Not found ansible-galaxy command..."
+    exit 1
+fi
+
+if type vagrant > /dev/null 2>&1
+then
+    vagrant up
+else
+    echo "Not found vagrant command..."
+    exit 1
+fi
